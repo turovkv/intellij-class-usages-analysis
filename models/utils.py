c7f3601e7c960423c01ebbddd23e92ca725f6303
Kirill Turov <turovkv@yandex.ru>
2022-06-14 22:29:39 +0300

    update baseline

--- models/utils.py
+++ models/utils.py
@@ -5,20 +5,29 @@ import pandas as pd
 def show_rank_metrics(df_group: pd.DataFrame, df_proba: pd.DataFrame, df_true: pd.DataFrame, label=''):
     print(f'\n------{label}-------')
     df_metric = pd.concat([df_group, df_proba, df_true], axis=1)
-    df_metric = df_metric.sort_values(by=['group', 'proba', 'correct'], ascending=[True, False, True])
+    #df_metric = df_metric.sort_values(by=['group', 'proba', 'correct'], ascending=[True, False, True])
+    df_metric = df_metric.sort_values(by=['group', 'proba'], ascending=[True, False])
     print(df_metric.shape)
     positions = []
+    zeros = []
+    group_size = []
     cur_group = -1
     cur_pos = 1
+    cur_prob_zeros = 0
     for row in df_metric.itertuples():
         cur_pos += 1
         if cur_group != row.group:
             cur_group = row.group
+            zeros.append(cur_prob_zeros)
+            group_size.append(cur_pos)
+            cur_prob_zeros = 0
             cur_pos = 1
+        if row.proba == 0:
+            cur_prob_zeros += 1
         if row.correct == 1:
             positions.append(cur_pos)
 
-    print(f'\nmean = {np.mean(positions)}\n')
+    print(f'\nmean = {np.mean(positions)}  median={np.median(positions)}, zerosCountMean={np.mean(zeros)}, groupSizeMean={np.mean(group_size)}\n')
 
     count = [0] * (max(positions) + 1)
     for p in positions:
@@ -31,6 +40,8 @@ def show_rank_metrics(df_group: pd.DataFrame, df_proba: pd.DataFrame, df_true: p
         if i > 15:
             print(f'stop {i}')
             break
+    # positions.sort()
+    # print(f'first 1000: {positions[:1000]}')
     print()
 
 #  example
